905f6102e91206d0888817707e3997bf747c99e3
camilolondonot <camilo.londono@bigholding.com.co>
2024-01-03 06:27:29 -0500

    primera entrega

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,4 @@ import windiCSS from 'vite-plugin-windicss';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), windiCSS()],
-  base: "/mandounificado-spiderfoft/",
 })
