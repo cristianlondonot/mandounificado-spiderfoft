758a5b7c6c071afdc0f714b3be9be61994693292
camilolondonot <camilo.londono@bigholding.com.co>
2024-01-03 13:40:37 -0500

    deploy build

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import windiCSS from 'vite-plugin-windicss';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), windiCSS()],
+  base: "/mandounificado-spiderfoft/"
 })
